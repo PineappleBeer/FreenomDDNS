0f5b4a4882b3c169947261650bd9b872d2a0fda9
PineappleBeer <zjyym@vip.qq.com>
2021-12-09 13:45:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Freenom DDNS
+
+一个可以自动更新ip到Freenom的shell。
+
+# 介绍
+
+DDNS服务很多，但得到的都是二级域名，这样有的时候可能不方便。
+
+如果家里有公网IP，又有[Freenom](https://freenom.com/)免费域名，为何不利用起来呢。
+
+众所周知，DDNS服务商更新IP的原理其实就是：
+
+请求一个获取IP的URL **&rarr;** 获取到IP **&rarr;** 将IP放到DDNS更新URL **&rarr;** 向连接发送一个get请求
+
+例如[dynv6](https://dynv6.com/)的更新URL是这样的
+
+      http://dynv6.com/api/update?hostname=<domain>&token=<username>&ipv4=<ipaddr>
+
+
+所以如果获取到[Freenom](https://freenom.com/)更新IP的URL及参数，那么就可以通过cURL像更新DDNS一样轻松更新更新IP地址了。
+
+# 配置
+
+`freenom_email` = Freenom账号
+
+`freenom_passwd` = Freenom密码
+
+`freenom_domain_name` = 需要更新的Freenom域名（当然，这必须是账号里的域名）
+
+`freenom_domain_id` = 目标域名的ID，这个可以在管理界面的链接里找到（不懂可以翻看下面）
+
+倒数第四行的`/scripts/shell/tgpush.sh` 这是一个使用tg bot推送消息的shell，通知运行结果，可以在[TelegramBotPush](https://github.com/PineappleBeer/TelegramBotPush)里了解。
+
+# 获取Freenom Domain ID
+
+首先打开[管理界面](https://my.freenom.com/clientarea.php?action=domains)
+
+<img src="./img/img1.png" width=60%>
+
+点击`Manage Domain`进去及就可以看到
+
+<img src="./img/img2.png" width=60%>
+
+地址栏的`id`就是我们要的ID了
+
+# cURL命令详解
+`--compressed`：curl自动解压缩响应页面（页面是压缩的话，例如目标服务器设置了gzip压缩了页面）。
+
+`-k`：允许不使用证书到SSL站点
+
+`-L `：当页面需要重定向时，curl自动跟踪重定向响应
+
+`-e `：服务端看到的请求来源地址
+
+`-c`：请求成功后，将cookie保存到目标文件
+
+`-F`：模拟HTTP表单提交
+
+`-b`：cookie读取位置
---
+++ freenom_ddns.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -u
+ 
+# settings
+# Login information of freenom.com
+freenom_email="00000000@gmail.com"
+freenom_passwd="00000000"
+# Open DNS management page in your browser.
+# URL vs settings:
+#   https://my.freenom.com/clientarea.php?managedns={freenom_domain_name}&domainid={freenom_domain_id}
+freenom_domain_name="000000.tk"
+freenom_domain_id="000000000"
+BASE_URL="https://my.freenom.com"
+CAREA_URL="$BASE_URL/clientarea.php"
+LOGIN_URL="$BASE_URL/dologin.php"
+LOGOUT_URL="$BASE_URL/logout.php"
+MANAGED_URL="$CAREA_URL?managedns=$freenom_domain_name&domainid=$freenom_domain_id"
+GET_IP_URL="https://api.ipify.org/"
+ 
+# get current ip address
+current_ip="$(curl -s "$GET_IP_URL")"
+ 
+if [ -z "$current_ip" ]; then
+    tgMsg="($freenom_domain_name) Could not get current IP address."
+    exit 1
+fi
+ 
+cookie_file=$(mktemp)
+cleanup() {
+    echo "Cleanup"
+    rm -f "$cookie_file"
+}
+trap cleanup INT EXIT TERM
+ 
+echo "Login"
+loginResult=$(curl --compressed -k -L -c "$cookie_file" \
+                   -F "username=$freenom_email" -F "password=$freenom_passwd" \
+                   -e "$CAREA_URL" \
+                   "$LOGIN_URL" 2>&1)
+ 
+if [ ! -s "$cookie_file" ]; then
+    tgMsg="($freenom_domain_name) Login failed: empty cookie."
+    exit 1
+fi
+ 
+if echo "$loginResult" | grep -q "/clientarea.php?incorrect=true"; then
+    tgMsg="($freenom_domain_name) Login failed."
+    exit 1
+fi
+ 
+echo "Update $current_ip to domain($freenom_domain_name)"
+updateResult=$(curl --compressed -k -L -b "$cookie_file" \
+                    -e "$MANAGED_URL" \
+                    -F "dnsaction=modify" \
+                    -F "records[0][line]=" \
+                    -F "records[0][type]=A" \
+                    -F "records[0][name]=" \
+                    -F "records[0][ttl]=14440" \
+                    -F "records[0][value]=$current_ip" \
+                    "$MANAGED_URL" 2>&1)
+
+ 
+if ! echo "$updateResult" | grep -q "name=\"records\[0\]\[value\]\" value=\"$current_ip\""; then
+    tgMsg="Update $current_ip to domain ( $freenom_domain_name ) faild!"
+    exit 1
+else
+    tgMsg="Update $current_ip to domain ( $freenom_domain_name ) successfully!"
+fi
+
+bash /scripts/shell/tgpush.sh "$tgMsg"
+
+echo "Logout"
+
+curl --compressed -k -b "$cookie_file" "$LOGOUT_URL" > /dev/null 2>&1
+ 
+exit 0
